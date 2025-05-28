6a5f03c3bad08719d5979f7fe666108acf41d385
Harsh-Gupta-1 <harshgupta2706@gmail.com>
2025-05-28 22:58:10 +0530

    Fixed executiveAPI

--- Client/src/api/executeAPI.js
+++ Client/src/api/executeAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const backendURL = import.meta.env.VITE_BACKEND_URL;
+const backendURL = import.meta.env.VITE_BACKEND_URL + "/api/execute";
 export const executeCode = async (code) => {
   const res = await axios.post(backendURL, { code });
   return res.data;
